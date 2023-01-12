7002b84a782ab31b2abf7a44ca026b6b62c2a50c
snagwon <ezlsw@naver.com>
2023-01-12 20:48:55 +0900

    bucket 연습

---
+++ app.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template, request, jsonify
+app = Flask(__name__)
+
+from pymongo import MongoClient
+client = MongoClient('mongodb+srv://test:sparta@cluster0.a1shctu.mongodb.net/Cluster0?retryWrites=true&w=majority')
+db = client.dbsparta
+
+@app.route('/')
+def home():
+   return render_template('index.html')
+
+@app.route("/bucket", methods=["POST"])
+def bucket_post():
+    sample_receive = request.form['sample_give']
+    print(sample_receive)
+    return jsonify({'msg': 'POST(기록) 연결 완료!'})
+
+@app.route("/bucket/done", methods=["POST"])
+def bucket_done():
+    bucket_receive = request.form['bucket_give']
+
+    bucket_list = list(db.users.find({}, {'_id': False}))
+    count = len(bucket_list)
+
+    doc = {
+        'num':count,
+        'bucket':bucket_receive,
+        'dome':0
+
+        }
+    db.bucket.insert_one(doc)
+    return jsonify({'msg': 'POST(완료) 연결 완료!'})
+
+@app.route("/bucket", methods=["GET"])
+def bucket_get():
+    return jsonify({'msg': 'GET 연결 완료!'})
+
+if __name__ == '__main__':
+   app.run('0.0.0.0', port=5000, debug=True)
